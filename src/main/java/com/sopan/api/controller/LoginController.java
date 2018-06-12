1986b7119d73850ed5098aa4871e78877c41e2c7
sopanlavhale <sopanlavhale@gmail.com>
2018-06-12 23:06:42 +0530

    Test hook

--- src/main/java/com/sopan/api/controller/LoginController.java
+++ src/main/java/com/sopan/api/controller/LoginController.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
+	
 	@RequestMapping("/login")
 	public String login() {
 		return "login";
+		
 	}
 }
